1cadcf49cb8ef7bf1a4bf1fb07cb7f0ffbdbea5c
Eruanno <maciek.baginski@wp.pl>
2021-10-21 01:30:56 +0200

    Sekcja 15: Spring Security

---
+++ src/main/java/io/github/eruanno/security/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package io.github.eruanno.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+        simpleAuthorityMapper.setPrefix("ROLE_");
+        simpleAuthorityMapper.setConvertToUpperCase(true);
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+        authenticationManagerBuilder.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        super.configure(httpSecurity);
+        httpSecurity.authorizeRequests()
+                .antMatchers("/info/*")
+                .hasRole("USER")
+                .antMatchers("/projects")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .permitAll();
+    }
+}
